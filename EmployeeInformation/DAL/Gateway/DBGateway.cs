a63b141ef9c06d1f40716273f0edb9fca5b4de61
MdmahmudulHasan <mahmud.doel@gmail.com>
2015-05-23 15:59:47 +0600

    https://github.com/gittiggit/CityInformationAppInlineSql.git

---
+++ EmployeeInformation/DAL/Gateway/DBGateway.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace EmployeeInformation.DAL.Gateway
+{
+    public class DBGateway
+    {
+        private SqlConnection connectionObj;
+        private SqlCommand commandObj;
+        
+        public SqlConnection ConnectionObj
+        {
+            get
+            {
+                return connectionObj;
+            }
+        }
+
+        public SqlCommand CommandObj
+        {
+            get
+            {
+                commandObj.Connection = connectionObj;
+                return commandObj;
+            }
+        }
+        public DBGateway()
+        {
+            connectionObj = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeConnectionString"].ConnectionString);
+            commandObj = new SqlCommand();
+        }
+
+
+
+
+    }
+}
